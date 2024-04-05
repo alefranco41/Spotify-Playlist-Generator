36179fe6586428328b7bbc231c44a97d89e89e53
alefranco41 <alessandro.franceschini.2002@gmail.com>
2024-04-05 20:57:48 +0200

    credentials management + custom day and hour arguments

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Installation Instructions:
   b) after you have verified the email,  wait about five minutes: Spotify takes time to register your accounts to the Developer Dashboard.
 3) Go to https://developer.spotify.com/dashboard and create up to five application for each Spotify account.
 4) For every application that you create, you have to insert the credentials 'client_id', 'client_secret' and 'redirect_uri' in the dictionary 'credentials_dicts' inside the module 'listening_history_manager'. Each key of 'credentials_dicts' is the email associated to a Spotify account, and each value is a list of (up to five) application credentials associated to that account. The more applications you create, the lower will be the chances of reaching the API request limit imposed by Spofify. Indeed, every time the software is used, the credentials of a random application are chosen. 
-5) (optional) you can change the variables 'current_day' and 'current_hour' inside the module 'playlist_generator' in order to generate a playlist for a different time of the day.
+5) (optional) you can change the variables 'current_day' and 'current_hour' by passing the options -d Day and -h Hour to the module 'playlist_generator' in order to generate a playlist for a different time of the day.
 6) (optional) you can comment the line 79 of the module 'playlist_generator' to generate (and upload on Spotify) only the playlist generated with our method.
 7) Run the module 'playlist_generator'. Whenever you are prompted a redirect URL, you have to:
   a) paste the URL into a browser
--- playlist_generator.py
+++ playlist_generator.py
@@ -5,19 +5,58 @@ import listening_history_manager
 import evaluation
 import sys
 from datetime import datetime
-current_hour = 9 #int(datetime.now().hour)
-current_day =  "Wednesday" #datetime.now().strftime("%A")
 
+def parse_arguments():
+    valid_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    valid_hours = [i for i in range(0, 24)]
+    args = sys.argv[1:]
 
-def experimental_phase(ans, prefix_name, current_hour_listening_history, periods, our_method_playlists, spotify):
+    current_day = None
+    current_hour = None
+    listening_history_file = None
+    step2 = False
+    skip_existing_playlists = False
+
+    for i in range(len(args)-1):
+        if args[i] == "-d" and args[i+1] in valid_days:
+            current_day = args[i+1]
+        if args[i] == "-h" and int(args[i+1]) in valid_hours:
+            current_hour = int(args[i+1])
+        if args[i] == "-f":
+            listening_history_file = args[i+1]
+        if args[i] == "--force-step-2" or (i == len(args)-2 and args[i+1] == "--force-step-2"):
+            step2 = True
+        if args[i] == "--skip-existing-playlists" or (i == len(args)-2 and args[i+1] == "--skip-existing-playlists"):
+            skip_existing_playlists = True
+
+    if not current_day:
+        current_day =  datetime.now().strftime("%A")
+    if current_hour is None:
+        current_hour = int(datetime.now().hour)
+        
+    if skip_existing_playlists and listening_history_file:
+        prefix_name = step1.compute_prefix_name(listening_history_file)
+        playlists = evaluation.retrieve_playlists()
+        if playlists and playlists.get((prefix_name,current_day,current_hour,"our_method"), None):
+            playlist_rec_1 = playlists.get((prefix_name,current_day,current_hour,"rec-1"), None)
+            playlist_rec_2 = playlists.get((prefix_name,current_day,current_hour,"rec-2"), None)
+            playlist_hyb_1 = playlists.get((prefix_name,current_day,current_hour,"hyb-1"), None)
+            if playlist_rec_1 is not None or playlist_rec_2 is not None or playlist_hyb_1 is not None:
+                print(f"Playlists already found for day: {current_day}, hour: {current_hour}, listening history: {listening_history_file}")
+                sys.exit()
+    return current_day, current_hour, listening_history_file, step2
+
+def experimental_phase(ans, prefix_name, current_hour_listening_history, periods, our_method_playlists, spotify, current_day):
     if ans != 'y':
         spotify = listening_history_manager.change_credentials()
 
     playlists_rec_1 = other_methods.get_rec_1_recommendations(current_hour_listening_history,prefix_name, current_day, spotify)
+    spotify = listening_history_manager.change_credentials()
     playlists_rec_2 = other_methods.get_rec_2_recommendations(current_hour_listening_history,prefix_name, current_day, spotify)
+    spotify = listening_history_manager.change_credentials()
     playlists_hyb_1, best_history_patterns = other_methods.get_hyb_1_recommendations(current_hour_listening_history, periods,prefix_name, current_day, spotify)
     other_methods_playlists = other_methods.create_playlists_dict(playlists_rec_1, playlists_rec_2, playlists_hyb_1, best_history_patterns, prefix_name, current_day)
-    step2.create_playlists(other_methods_playlists, spotify)
+    #step2.create_playlists(other_methods_playlists, spotify)
     
     #experimental phase
     all_playlists = {**our_method_playlists, **other_methods_playlists}
@@ -26,10 +65,13 @@ def experimental_phase(ans, prefix_name, current_hour_listening_history, periods
     evaluation.generate_spreadsheet(results)
 
 def main():
+    current_day, current_hour, listening_history_file, force_step2 = parse_arguments()
     spotify = listening_history_manager.change_credentials()
     listening_history_file_data = []
-    #listening_history_file = input(step1.input_message)
-    listening_history_file = "listening_histories/LH1.csv"
+    
+    if not listening_history_file:
+        listening_history_file = input(step1.input_message)
+
     if listening_history_file:
         listening_history_file_data = step1.csv_to_dict(listening_history_file)
     
@@ -56,13 +98,15 @@ def main():
     
     most_similar_song_sets = step2.retrieve_most_similar_song_set(prefix_name, current_hour)
     ans = None
-    if most_similar_song_sets:
+    if most_similar_song_sets and not force_step2:
         ans = input(f"A file containing the most similar song set for hour {current_hour} has been found, would you like to skip to step 2? (y/n): ")
         if ans != 'y':
             most_similar_song_sets = None
+    elif most_similar_song_sets and force_step2:
+        ans = 'y'
     
     #step 1
-    if not most_similar_song_sets:        
+    if not most_similar_song_sets or ans != 'y':        
         clusterings = step1.compute_clusterings(current_hour_listening_history)
         clustering_song_sets = step1.generate_clustering_song_sets(clusterings, spotify)
         most_similar_song_sets = step1.compute_most_similar_song_sets(clustering_song_sets, periods, current_hour_listening_habits)
@@ -74,10 +118,10 @@ def main():
     optimal_solutions_indexes = step2.compute_optimal_solution_indexes(history_patterns, most_similar_song_sets, spotify)
     our_method_playlists = step2.retrieve_optimal_solution_songs(optimal_solutions_indexes, most_similar_song_sets, spotify)
     our_method_playlists = step2.create_playlists_dict(our_method_playlists, current_day, history_patterns, prefix_name)
-    step2.create_playlists(our_method_playlists, spotify)
+    #step2.create_playlists(our_method_playlists, spotify)
 
     #other methods
-    experimental_phase(ans, prefix_name, current_hour_listening_history, periods, our_method_playlists, spotify)
+    experimental_phase(ans, prefix_name, current_hour_listening_history, periods, our_method_playlists, spotify, current_day)
 
         
 if __name__ == "__main__":
